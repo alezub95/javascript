575f4130de4f97146157024df1792ba9a9cf9802
Aleksandra Zubrzak <aleksandra.zubrzak@gmail.com>
2018-04-22 16:51:25 +0200

    war

---
+++ js/warsztat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function wynikIloczynu(para1, para2, para3) {
+
+    var iloczyn = para1 * para2 * para3;
+
+    return iloczyn;
+}
+
+
+var zmienna = wynikIloczynu(2, 3, 4);
+console.log(zmienna);
